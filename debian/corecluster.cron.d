4d63697360250a7ae5fdd74446e515541627529d
Maciej Nabozny <maciej.nabozny@cloudover.io>
2016-11-14 12:57:57 +0100

    Debian package

--- debian/corecluster.cron.d
+++
@@ -1,5 +0,0 @@
-* *     * * *	root	[ -x /usr/sbin/cc-manage ] && /usr/sbin/cc-manage cron minute
-1 *     * * *	root	[ -x /usr/sbin/cc-manage ] && /usr/sbin/cc-manage cron hourly
-22 1    * * *	root	[ -x /usr/sbin/cc-manage ] && /usr/sbin/cc-manage cron daily
-31 3    * * 7	root	[ -x /usr/sbin/cc-manage ] && /usr/sbin/cc-manage cron weekly
-22 4    1 * *	root	[ -x /usr/sbin/cc-manage ] && /usr/sbin/cc-manage cron monthly
