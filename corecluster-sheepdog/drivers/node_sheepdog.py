0169d9042971d32664cfcec21b583e630a4f0780
Maciej Nabozny <maciej.nabozny@cloudover.io>
2016-11-14 12:48:38 +0100

    Initialized with sheepdog tools

---
+++ corecluster-sheepdog/drivers/node_sheepdog.py
@@ -0,0 +1,43 @@
+"""
+Copyright (c) 2015 Maciej Nabozny
+              2015 Marta Nabozny
+
+This file is part of CloudOver project.
+
+CloudOver is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import time
+from corenetwork.utils import system
+from corenetwork.utils.logger import log
+from corenetwork.drivers.node_default import Driver as DefaultNodeDriver
+
+
+class Driver(DefaultNodeDriver):
+    def _sheepdog_startup(self):
+        for i in xrange(30):
+            r = system.call('dog node list', shell=True)
+            if r == 0:
+                break
+            else:
+                log(msg="sheepdog_startup: Restarting shepdog service", tags=('system', 'info'))
+                system.call('service sheepdog restart', shell=True)
+                time.sleep(i)
+
+    def startup_node(self):
+        """
+        This method is called when node is started, by cc-node
+        """
+        super(Driver, self).startup_node()
+        self._sheepdog_startup()
